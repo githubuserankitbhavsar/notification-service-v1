f1c558936e2c4144d8cc735a5f60c0eb643286bf
Ankit Bhavsar <bhavsarankit.java@gmail.com>
2023-10-28 09:21:29 +0530

    initial commit with base files.

---
+++ src/main/java/com/scaler/training/localbusiness/notificationservicev1/NotificationServiceApplication.java
@@ -0,0 +1,16 @@
+package com.scaler.training.localbusiness.notificationservicev1;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableRabbit
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/training/localbusiness/notificationservicev1/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.training.localbusiness.notificationservicev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
